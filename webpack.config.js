57563d40dd60b4c54db15874cc719a0c28b3d4ed
Carlos <84628913+MaGaiDeN@users.noreply.github.com>
2024-11-09 23:30:10 +0100

    Agrega logo y corrige configuración de webpack para imágenes

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: './'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
+        clean: true
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -32,10 +16,18 @@ module.exports = {
         new CopyWebpackPlugin({
             patterns: [
                 { 
-                    from: './images',
-                    to: 'images'
+                    from: path.resolve(__dirname, 'images'),
+                    to: path.resolve(__dirname, 'dist/images')
                 }
             ]
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
 }; 
